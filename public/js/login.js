009946852087c89a9d6d24d4f21ea89065014bfa
Zheng Yang Tean <teanzhengyang@outlook.sg>
2018-05-19 13:28:33 +0800

    login page

---
+++ public/js/login.js
@@ -0,0 +1,14 @@
+function showHidePassword() {
+    var x = document.getElementById("passwordInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+$(document).ready(function(){
+    $( "#loginButton" ).click(function() {
+      window.top.location.href = "/user-profile.html"; 
+    });
+});
