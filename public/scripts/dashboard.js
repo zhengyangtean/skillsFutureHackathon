7728df4d5911abcb721d17fda6a3cb056f6fd2e7
Abdul Hadi <damienhadios@gmail.com>
2018-05-19 17:55:35 +0800

    update script.

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -4,30 +4,8 @@ $(document).ready(() => {
 
 
 function setupClickHandler () {
-    // Hide all the save btn
-    $(".save-btn").hide();
-    
-    $(".edit-btn").click((e) => {
-        const content = $(e.target).closest(".progress-topic-content");
-        
-        content.toggleClass("active");
-        content.find("textarea").removeAttr("disabled", "dis");
-        
-        // Hide the save and submit btn
-        content.find(".submit-btn, .edit-btn").hide();
-        content.find(".save-btn").show();
-        
-        content.find("textarea").focus();
-    });
-    
-    $(".save-btn").click((e) => {
-        const content = $(e.target).closest(".progress-topic-content");
-        
-        content.toggleClass("active");
-        content.find("textarea").attr("disabled", "disabled");
-        
-        // Hide the save and submit btn
-        content.find(".submit-btn, .edit-btn").show();
-        content.find(".save-btn").hide();
+    $(".topic-item").click((e) => {
+        const content = $(e.target).closest(".topic-item").find(".topic-content");
+        content.toggle();
     });
 }
