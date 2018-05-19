d241d6b53e38e951549886265a18d9983c10a9f6
Abdul Hadi <damienhadios@gmail.com>
2018-05-19 12:32:02 +0800

    Add browser sync for auto refresh.

---
+++ .bs-config.js
@@ -0,0 +1,51 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "files": ["public/*.css", "public/*.html"],
+    "watchEvents": [
+        "change"
+    ],
+    "watchOptions": {
+        "ignoreInitial": true
+    },
+    "server": {
+        baseDir: "public"
+    },
+    "port": 3001,
+    "serveStatic": ["./public/*.html"],
+    "logLevel": "info",
+    "logPrefix": "BS",
+    "logConnections": true,
+    "logFileChanges": true,
+    "logSnippet": true,
+    "rewriteRules": [],
+    "open": "external",
+    "browser": "default",
+    "reloadOnRestart": false,
+    "notify": true,
+    "minify": true,
+    "host": "localhost",
+    "codeSync": true,
+    "timestamps": true,
+    "clientEvents": [
+        "scroll",
+        "scroll:element",
+        "input:text",
+        "input:toggles",
+        "form:submit",
+        "form:reset",
+        "click"
+    ],
+};
